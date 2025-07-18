946302b9dff34ea602282b0ca9b1f19e8ec28328
Moeein Aali <moeeeinaali@gmail.com>
2025-07-18 10:34:33 +0330

    [FIX] colors and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <p className={"m-auto mt-8 font-bold text-white text-5xl text-center"}>
-        Home Page
+        صفحه‌ی اصلی
       </p>
     </>
   );
