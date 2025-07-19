b2e19106082b6b205ae4b914842754ddddb46f5f
Moeein Aali <moeeeinaali@gmail.com>
2025-07-19 16:02:49 +0330

    [FEAT] Add middleware matcher

--- src/app/core/auth.ts
+++ src/app/core/auth.ts
@@ -1,78 +1,80 @@
-import {NextRequest, NextResponse} from "next/server";
-import {decryptSession} from "@/app/utils/session";
-import {cookies} from "next/headers";
-import {setAuthCookieAction} from "@/app/_actions/auth.action";
-import {UserResponse} from "@/app/_types/auth.types";
+import { NextRequest, NextResponse } from "next/server";
+import { decryptSession } from "@/app/utils/session";
+import { cookies } from "next/headers";
+import { setAuthCookieAction } from "@/app/_actions/auth.action";
+import { UserResponse } from "@/app/_types/auth.types";
+
+const authRoutes = ["/signin"];
+const protectedRoutes = ["/dashboard"];
 
 export async function authMiddleware(request: NextRequest) {
-    const sessionCookie = request.cookies.get("uranus-session")?.value;
+  const signInRoute = request.nextUrl.clone();
+  signInRoute.pathname = "/signin";
+  const { nextUrl } = request;
+  const nextResponse = NextResponse.next();
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    nextUrl.pathname.startsWith(route),
+  );
 
-    const authRoutes = ["/signin"];
-    const protectedRoutes = ["/dashboard"];
-    const signInRoute = request.nextUrl.clone();
-    signInRoute.pathname = "/signin";
-
-    const {nextUrl} = request;
-    const nextResponse = NextResponse.next();
-
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-    const isProtectedRoute = protectedRoutes.some((route) =>
-        nextUrl.pathname.startsWith(route),
-    );
-    if (!sessionCookie) {
-        if (isProtectedRoute) {
-            const callback = encodeURIComponent(nextUrl.pathname);
-            return NextResponse.redirect(`${signInRoute}?callback=${callback}`);
-        } else {
-            return nextResponse;
-        }
-    }
-    try {
-        const parsed = await decryptSession(JSON.parse(sessionCookie));
-        const now = Date.now();
-
-        const accessTokenExpired = parsed.exp < now;
-        const refreshTokenExpired = parsed.sessionExpiry < now - 30 * 1000;
-
-        if (!accessTokenExpired && !refreshTokenExpired && isAuthRoute) {
-            const dashboardRoute = request.nextUrl.clone();
-            dashboardRoute.pathname = "/dashboard/courses";
-            return NextResponse.redirect(dashboardRoute);
-        }
-
-        if (refreshTokenExpired) {
-            const cookieStore = await cookies()
-            cookieStore.delete('uranus-session')
-            return NextResponse.redirect(signInRoute)
-        }
-
-        if (accessTokenExpired && !refreshTokenExpired) {
-            try {
-                const response = await fetch(
-                    `${process.env.NEXT_PUBLIC_API}/api/identity/refresh-token`,
-                    {
-                        method: "POST",
-                        body: JSON.stringify({sessionId: parsed.sessionId}),
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    },
-                );
-
-                if (response.ok) {
-                    const user = await response.json() as unknown as UserResponse;
-                    await setAuthCookieAction(user)
-                }
-
-            } catch {
-                return NextResponse.redirect(signInRoute)
-            }
-        }
-
-
-    } catch {
-        return NextResponse.redirect(signInRoute);
-    }
-
+  if (!isProtectedRoute && !isAuthRoute) {
     return nextResponse;
+  }
+
+  const sessionCookie = request.cookies.get("uranus-session")?.value;
+
+  if (!sessionCookie) {
+    if (isProtectedRoute) {
+      const callback = encodeURIComponent(nextUrl.pathname);
+      return NextResponse.redirect(`${signInRoute}?callback=${callback}`);
+    } else {
+      return nextResponse;
+    }
+  }
+
+  try {
+    const parsed = await decryptSession(JSON.parse(sessionCookie));
+    const now = Date.now();
+
+    const accessTokenExpired = parsed.exp < now;
+    const refreshTokenExpired = parsed.sessionExpiry < now - 30 * 1000;
+
+    if (!accessTokenExpired && !refreshTokenExpired && isAuthRoute) {
+      const dashboardRoute = request.nextUrl.clone();
+      dashboardRoute.pathname = "/dashboard/courses";
+      return NextResponse.redirect(dashboardRoute);
+    }
+
+    if (refreshTokenExpired) {
+      const cookieStore = await cookies();
+      cookieStore.delete("uranus-session");
+      return NextResponse.redirect(signInRoute);
+    }
+
+    if (accessTokenExpired && !refreshTokenExpired) {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API}/api/identity/refresh-token`,
+          {
+            method: "POST",
+            body: JSON.stringify({ sessionId: parsed.sessionId }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        );
+
+        if (response.ok) {
+          const user = (await response.json()) as unknown as UserResponse;
+          await setAuthCookieAction(user);
+        }
+      } catch {
+        return NextResponse.redirect(signInRoute);
+      }
+    }
+  } catch {
+    return NextResponse.redirect(signInRoute);
+  }
+
+  return nextResponse;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,3 +4,9 @@ import { authMiddleware } from "@/app/core/auth";
 export function middleware(request: NextRequest) {
   return authMiddleware(request);
 }
+
+export const config = {
+  matcher: [
+    "/((?!_next|favicon.ico|robots.txt|sitemap.xml|api|static|assets|.*\\..*).*)",
+  ],
+};
